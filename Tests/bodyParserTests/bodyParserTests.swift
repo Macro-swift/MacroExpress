84863931545b06b50607e9de42f650d4d2883e18
Helge Heß <me@helgehess.eu>
2020-07-30 22:27:31 +0200

    Attempt to test concat body parsing
    This isn't working right yet w/ a plain `push`. Probably
    some ordering issue.

---
+++ Tests/bodyParserTests/bodyParserTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+import Macro
+@testable import http
+@testable import connect
+
+final class bodyParserTests: XCTestCase {
+  
+  func testStringParser() throws {
+    let req = IncomingMessage(
+      .init(version: .init(major: 1, minor: 1),
+            method: .POST, uri: "/post", headers: [
+              "Content-Type": "text/html"
+            ])
+    )
+
+    req.push(Buffer("Hello World"))
+
+    let res = ServerResponse(unsafeChannel: nil, log: req.log)
+    
+    // this is using concat ... so we need an expectations
+    let sem = expectation(description: "parsing body ...")
+    let mw = bodyParser.text()
+    try mw(req, res) { ( args : Any...) in
+      console.log("done parsing ...", args)
+      sem.fulfill()
+    }
+
+
+    waitForExpectations(timeout: 3) { error in
+      if let error = error {
+        console.log("Error:", error.localizedDescription)
+      }
+      console.log("REQ:", req.body)
+    }
+  }
+
+  static var allTests = [
+    ( "testStringParser", testStringParser )
+  ]
+}
