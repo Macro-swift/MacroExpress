36a717103a3c9c12e89ad0845d4f76debe975011
Helge Heß <helge@alwaysrightinstitute.com>
2020-12-04 12:38:04 +0100

    Revert "Refactor DotEnv"

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,11 +1,8 @@
 import XCTest
 import mimeTests
 import routeTests
-import dotenvTests
 
 var tests = [ XCTestCaseEntry ]()
 tests += mimeTests.allTests()
 tests += routeTests.allTests()
-tests += dotenvTests.allTests
-
 XCTMain(tests)
