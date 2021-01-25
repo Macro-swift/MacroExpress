120540168d72160e997a079860b99b58f49f4db4
Helge Heß <devteam@zeezide.de>
2021-01-25 14:22:28 +0100

    Test body data returned by parser
    Which is still wrong, setting expectations is
    important!

--- Tests/bodyParserTests/MultiPartParserTests.swift
+++ Tests/bodyParserTests/MultiPartParserTests.swift
@@ -8,6 +8,17 @@ final class MultiPartParserTests: XCTestCase {
   func testSimpleFormData() throws {
     let boundary = "----WebKitFormBoundaryHU6Dqpfe9L4ATppg"
     
+    let part1Header = [
+      ( "Content-Disposition" , "form-data; name=\"title\"" )
+    ]
+    let part1Value = Buffer("file.csv")
+    
+    let part2Header = [
+      ( "Content-Disposition" , "form-data; name=\"file\"; filename=\"\"" ),
+      ( "Content-Type"        , "application/octet-stream" )
+    ]
+    let part2Value = Buffer()
+
     let requestBody = Buffer(
       """
       --\(boundary)\r
@@ -33,11 +44,30 @@ final class MultiPartParserTests: XCTestCase {
       events.append($0)
     }
     
-    print("EVENTS:", events)
+    print("EVENTS:")
     XCTAssertFalse(events.isEmpty)
+    for event in events {
+      print("  -", event)
+    }
+    
+    XCTAssertEqual(events.count, 6)
+    XCTAssertEqual(events[opt: 0], .startPart(part1Header))
+    XCTAssertEqual(events[opt: 1], .bodyData(part1Value))
+    XCTAssertEqual(events[opt: 2], .endPart)
+    XCTAssertEqual(events[opt: 3], .startPart(part2Header))
+    XCTAssertEqual(events[opt: 4], .bodyData(part2Value))
+    XCTAssertEqual(events[opt: 5], .endPart)
   }
 
   static var allTests = [
     ( "testSimpleFormData" , testSimpleFormData ),
   ]
+}
+
+fileprivate extension Array {
+  
+  subscript(opt index: Int) -> Element? {
+    guard index >= 0 && index < count else { return nil }
+    return self[index]
+  }
 }
