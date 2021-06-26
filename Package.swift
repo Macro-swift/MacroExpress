650533146fa5409884709427c810940def9e8b53
Helge Heß <devteam@zeezide.de>
2021-06-26 16:57:26 +0200

    Use the latest Macro
    Only the latest is the greatest!

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
   
   dependencies: [
     .package(url: "https://github.com/Macro-swift/Macro.git",
-             from: "0.8.7"),
+             from: "0.8.8"),
     .package(url: "https://github.com/AlwaysRightInstitute/mustache.git",
              from: "0.5.9")
   ],
