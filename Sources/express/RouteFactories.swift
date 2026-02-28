e51797f654aeaaec79dc6513b8651bedf43829c1
Helge Heß <devteam@zeezide.de>
2026-02-28 16:00:37 +0100

    Add async middleware overloads for routes
    So we can do this now:
    ```swift
    app.get { req, res in
      try await Task.sleep(1_000_000) // <= invoke async code
      res.send("Done")
    }
    ```

--- Sources/express/RouteFactories.swift
+++ Sources/express/RouteFactories.swift
@@ -8,6 +8,9 @@
 
 import typealias connect.Middleware
 import typealias connect.FinalMiddleware
+import typealias connect.AsyncMiddleware
+import typealias connect.AsyncFinalMiddleware
+import func      connect.async
 import NIOHTTP1
 
 // TBD: All the duplication below looks a little stupid, is there a better way
@@ -450,6 +453,412 @@ public extension RouteKeeper {
   func patch(id: String? = nil, _ errorMiddleware: ErrorMiddleware...) -> Self {
     add(route: Route(id: id, pattern: nil, method: .PATCH,
                      errorMiddleware: errorMiddleware))
+    return self
+  }
+}
+
+
+// MARK: - Async Versions
+
+public extension RouteKeeper {
+
+  // MARK: - Use // All
+
+  @discardableResult
+  @inlinable
+  func use(id: String? = nil, _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: nil,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+
+  @discardableResult
+  @inlinable
+  func use(id: String? = nil, _ pathPattern: String,
+           _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: nil,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+
+  @discardableResult
+  @inlinable
+  func all(id: String? = nil, _ pathPattern: String,
+           _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: nil,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+
+  // MARK: - GET / POST / HEAD / PUT / DELETE / PATCH
+
+  @discardableResult
+  @inlinable
+  func get(id: String? = nil, _ pathPattern: String,
+           _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .GET,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func post(id: String? = nil, _ pathPattern: String,
+            _ middleware: Middleware...,
+            asyncMiddleware: [ AsyncMiddleware ] = [],
+            async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .POST,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func head(id: String? = nil, _ pathPattern: String,
+            _ middleware: Middleware...,
+            asyncMiddleware: [ AsyncMiddleware ] = [],
+            async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .HEAD,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func put(id: String? = nil, _ pathPattern: String,
+           _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .PUT,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func del(id: String? = nil, _ pathPattern: String,
+           _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .DELETE,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func patch(id: String? = nil, _ pathPattern: String,
+             _ middleware: Middleware...,
+             asyncMiddleware: [ AsyncMiddleware ] = [],
+             async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .PATCH,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+
+  @discardableResult
+  @inlinable
+  func get(id: String? = nil, _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .GET,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func post(id: String? = nil, _ middleware: Middleware...,
+            asyncMiddleware: [ AsyncMiddleware ] = [],
+            async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .POST,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func head(id: String? = nil, _ middleware: Middleware...,
+            asyncMiddleware: [ AsyncMiddleware ] = [],
+            async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .HEAD,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func put(id: String? = nil, _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .PUT,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func del(id: String? = nil, _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .DELETE,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func patch(id: String? = nil, _ middleware: Middleware...,
+             asyncMiddleware: [ AsyncMiddleware ] = [],
+             async lastMiddleware: @escaping AsyncMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .PATCH,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(lastMiddleware) ]))
+    return self
+  }
+}
+
+
+// MARK: - Async Final Versions
+
+public extension RouteKeeper {
+
+  // MARK: - Use // All
+
+  @discardableResult
+  @inlinable
+  func use(id: String? = nil, _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: nil,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+
+  @discardableResult
+  @inlinable
+  func use(id: String? = nil, _ pathPattern: String,
+           _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: nil,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+
+  @discardableResult
+  @inlinable
+  func all(id: String? = nil, _ pathPattern: String,
+           _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: nil,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+
+  // MARK: - GET / POST / HEAD / PUT / DELETE / PATCH
+
+  @discardableResult
+  @inlinable
+  func get(id: String? = nil, _ pathPattern: String,
+           _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .GET,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func post(id: String? = nil, _ pathPattern: String,
+            _ middleware: Middleware...,
+            asyncMiddleware: [ AsyncMiddleware ] = [],
+            asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .POST,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func head(id: String? = nil, _ pathPattern: String,
+            _ middleware: Middleware...,
+            asyncMiddleware: [ AsyncMiddleware ] = [],
+            asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .HEAD,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func put(id: String? = nil, _ pathPattern: String,
+           _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .PUT,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func del(id: String? = nil, _ pathPattern: String,
+           _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .DELETE,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func patch(id: String? = nil, _ pathPattern: String,
+             _ middleware: Middleware...,
+             asyncMiddleware: [ AsyncMiddleware ] = [],
+             asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: pathPattern, method: .PATCH,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+
+  @discardableResult
+  @inlinable
+  func get(id: String? = nil, _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .GET,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func post(id: String? = nil, _ middleware: Middleware...,
+            asyncMiddleware: [ AsyncMiddleware ] = [],
+            asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .POST,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func head(id: String? = nil, _ middleware: Middleware...,
+            asyncMiddleware: [ AsyncMiddleware ] = [],
+            asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .HEAD,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func put(id: String? = nil, _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .PUT,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func del(id: String? = nil, _ middleware: Middleware...,
+           asyncMiddleware: [ AsyncMiddleware ] = [],
+           asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .DELETE,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
+    return self
+  }
+  @discardableResult
+  @inlinable
+  func patch(id: String? = nil, _ middleware: Middleware...,
+             asyncMiddleware: [ AsyncMiddleware ] = [],
+             asyncFinal: @escaping AsyncFinalMiddleware) -> Self
+  {
+    add(route: Route(id: id, pattern: nil, method: .PATCH,
+                     middleware: middleware
+                       + asyncMiddleware.map(async)
+                       + [ async(asyncFinal) ]))
     return self
   }
 }
